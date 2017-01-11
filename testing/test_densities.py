da972ddf388ef898461b6441f8dfda110606abc5
fujiikeisuke <fujiisoup@gmail.com>
2017-01-11 14:35:21 +0900

    Cleanup student_t code.

--- testing/test_densities.py
+++ testing/test_densities.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 import numpy as np
 import unittest
 import Henbun as hb
+from scipy.special import loggamma
+from Henbun._settings import settings
+float_type = settings.dtypes.float_type
+np_float_type = np.float32 if float_type is tf.float32 else np.float64
 
 class test_bimixture(unittest.TestCase):
     def test(self):
@@ -18,6 +22,58 @@ class test_bimixture(unittest.TestCase):
 
         logp_np = np.log(frac*np.exp(logp0)+(1-frac)*np.exp(logp1))
         self.assertTrue(np.allclose(logp_mix, logp_np))
+
+def student_t_ref(x, mu, scale, nu):
+    """ numpy method for student t log density """
+    const = loggamma(0.5*(nu+1.0)) - loggamma(0.5*nu) \
+         - 0.5*(np.log(scale*scale) + np.log(nu) + np.log(np.pi))
+
+    return const - 0.5*(nu + 1.) *\
+        np.log(1.0 + (1.0/nu)*((x-mu)/scale)**2.0)
+
+class test_student_t(unittest.TestCase):
+    def test_np(self):
+        """ With numpy array parameter """
+        rng = np.random.RandomState(0)
+        x = rng.randn(2,3,4).astype(np_float_type)
+        mu = rng.randn(2,3,4).astype(np_float_type)
+        scale = np.exp(rng.randn(2,3,4).astype(np_float_type))
+        nu = np.exp(rng.randn(2,3,4).astype(np_float_type))
+
+        # scalar deg_free
+        logp_ref = student_t_ref(x, mu, scale, 3.0)
+        with tf.Session() as sess:
+            logp = sess.run(hb.densities.student_t(x, mu, scale, 3.0))
+        self.assertTrue(logp.dtype == 'float32')
+        self.assertTrue(np.allclose(logp, logp_ref, atol=1.0e-5))
+
+        # tensor_input
+        with tf.Session() as sess:
+            logp = sess.run(hb.densities.student_t(
+                tf.constant(x, float_type),
+                tf.constant(mu, float_type),
+                tf.constant(scale, float_type),
+                3.0))
+        self.assertTrue(logp.dtype == 'float32')
+        self.assertTrue(np.allclose(logp, logp_ref, atol=1.0e-5))
+
+        # tensor deg_free
+        logp_ref = student_t_ref(x, mu, scale, nu)
+        with tf.Session() as sess:
+            logp = sess.run(hb.densities.student_t(x, mu, scale, nu))
+        self.assertTrue(logp.dtype == 'float32')
+        self.assertTrue(np.allclose(logp, logp_ref, atol=1.0e-5))
+
+        # tensor_input
+        with tf.Session() as sess:
+            logp = sess.run(hb.densities.student_t(
+                tf.constant(x, float_type),
+                tf.constant(mu, float_type),
+                tf.constant(scale, float_type),
+                tf.constant(nu, float_type)))
+        self.assertTrue(logp.dtype == 'float32')
+        self.assertTrue(np.allclose(logp, logp_ref, atol=1.0e-5))
+
 
 if __name__ == '__main__':
     unittest.main()
